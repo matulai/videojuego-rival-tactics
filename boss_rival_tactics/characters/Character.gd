25c1422e2147c6c8a5e4273958fd0779e14ad0b4
Matias Laime <matulai1221@gmail.com>
2024-10-04 16:14:14 -0300

    Se modifica el take damage debido que se creo el autoload

--- boss_rival_tactics/characters/Character.gd
+++ boss_rival_tactics/characters/Character.gd
@@ -1,15 +1,21 @@
 extends CharacterBody2D
 class_name Character
 
+@export var isTeamOne: bool
+
 @export var speed: int = 200
 
 @export var max_hp : int = 20
 @export var hp: int = 2: set = set_hp
 #signal hp_changed(new_hp)
 
-@onready var animated_sprite: AnimatedSprite2D = get_node("AnimatedSprite2D")
+@onready var sprite: Sprite2D = get_node("Sprite2D")
+@onready var animation_player: AnimationPlayer = get_node("AnimationPlayer")
 
 var move_direction: Vector2 = Vector2.ZERO
+
+func _ready() -> void:
+	set_motion_mode(1)
 
 func _physics_process(_delta: float) -> void:
 	move_and_slide()
@@ -28,8 +34,13 @@ func take_damage(amount, dir, force) -> void:
 		remove()
 		
 func remove() -> void:
+	GLOBAL.erase_unit_from_an_army(self, isTeamOne)
 	queue_free()
 
 func set_hp(new_hp: int) -> void:
 	hp = clamp(new_hp, 0, max_hp)
 	#emit_signal("hp_changed", hp)
+
+func _play_animation(animation: String) -> void:
+	if animation_player.has_animation(animation):
+		animation_player.play(animation)
