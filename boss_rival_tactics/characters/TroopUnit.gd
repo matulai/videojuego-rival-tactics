77310cb9897f65576331b467931011e46979403e
Matias Laime <matulai1221@gmail.com>
2024-10-04 16:16:39 -0300

    Pathfinding funcionando, pero a que precio

--- boss_rival_tactics/characters/TroopUnit.gd
+++ boss_rival_tactics/characters/TroopUnit.gd
@@ -4,27 +4,35 @@ class_name TroopUnit
 @onready var navigation_agent: NavigationAgent2D = get_node("NavigationAgent2D")
 @onready var path_timer: Timer = get_node("PathTimer")
 
-#Establezco de donde ir a buscar los enemigos para el NavigationAgent2D
-@onready var enemiesNodes: Array[Node] = get_node("../../PlayerOneArmyNode").get_children()
-@onready var player: Node = get_node("../../PlayerOneArmyNode/Player")
+@export var damage: int = 1
+@export var knockback_force: int = 300
+@export var knockback_direction: Vector2 = Vector2.ZERO
+
+var target: Character = null
 
 func chase() -> void:
 	var vector_to_next_point: Vector2 = navigation_agent.get_next_path_position() - global_position
 	move_direction = vector_to_next_point
 
-func _get_path_to_player() -> void:
-	navigation_agent.target_position = player.position
+func _get_path_to_enemy() ->void:
+	navigation_agent.target_position = target.position
 
 func _on_path_timer_timeout() -> void:
-	print("player")
 #	Verifico si el player esta en memoria, si no esta es que fue eliminado y busco otro.
-	if is_instance_valid(player):
-		_get_path_to_player()
-		chase()
+	if is_instance_valid(target):
+		target = get_closest_node(GLOBAL.get_unit_enemies(isTeamOne))
+		_get_path_to_enemy()
 	else:
-		path_timer.stop()
-		move_direction = Vector2.ZERO
+		target = get_closest_node(GLOBAL.get_unit_enemies(isTeamOne))
 
-#func _get_path_to_enemy() -> void:
-##	PENSAR EN COMO MEJORAR LA ELECCION DE ENEMIGOS AL INICIO Y LUEGO CUANDO MUERE.
-	#navigation_agent.target_position = enemiesNodes[0].position
+func get_closest_node(nodes: Array) -> Node:
+	var closest_node: Node = null
+	var min_distance: float = INF
+	
+	for node in nodes:
+		if node and node is Node2D:  # Asegúrate de que el nodo es un Node2D
+			var distance = global_position.distance_to(node.global_position)
+			if distance < min_distance:
+				min_distance = distance
+				closest_node = node
+	return closest_node
