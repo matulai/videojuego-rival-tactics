591a15dc711ffcdbf1782109e57008d9baa23855
Matias Laime <matulai1221@gmail.com>
2024-10-28 19:56:17 -0300

    Se crea clase base para proyectiles

---
+++ boss_rival_tactics/weapons/Proyectile.gd
@@ -0,0 +1,36 @@
+extends Area2D
+
+@onready var lifetime_timer: Timer = $LifetimeTimer
+
+@export var VELOCITY: float = 0
+
+var direction: Vector2
+var damage: int
+var collision: int
+
+func initialize(initial_position: Vector2, dir: Vector2, speed: int, dam: int, angle: float, collision_mask: int) -> void:
+	position = initial_position
+	rotation += angle
+	damage = dam
+	direction = dir
+	VELOCITY = speed
+	collision = collision_mask
+
+func _ready() -> void:
+	lifetime_timer.start()
+	collision_mask = collision
+
+func _physics_process(delta: float) -> void:
+	position += direction * VELOCITY * delta
+
+func remove() -> void:
+	collision_mask = 0
+	queue_free()
+	
+func _on_lifetime_timer_timeout() -> void:
+	remove()
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("take_damage"):
+		body.take_damage(damage, 0, 0)
+	remove()
