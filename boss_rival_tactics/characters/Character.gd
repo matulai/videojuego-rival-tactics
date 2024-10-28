efea971ef8a6caeb64775e106841123a7b569e0c
Matias Laime <matulai1221@gmail.com>
2024-10-28 20:01:35 -0300

    Separacion en layers y mask

--- boss_rival_tactics/characters/Character.gd
+++ boss_rival_tactics/characters/Character.gd
@@ -16,6 +16,10 @@ var move_direction: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
 	set_motion_mode(1)
+	if isTeamOne:
+		collision_layer = 1
+	else:
+		collision_layer = 2
 
 func _physics_process(_delta: float) -> void:
 	move_and_slide()
@@ -24,15 +28,11 @@ func move() -> void:
 	move_direction = move_direction.normalized()
 	velocity = move_direction * speed
 
-######################################################
-
 func take_damage(amount, dir, force) -> void:
 	self.hp -= amount
-	if (hp > 0):
-		velocity += dir * force
-	else:
+	if (hp <= 0):
 		remove()
-		
+
 func remove() -> void:
 	GLOBAL.erase_unit_from_an_army(self, isTeamOne)
 	queue_free()
--- boss_rival_tactics/characters/TroopUnit.gd
+++ boss_rival_tactics/characters/TroopUnit.gd
@@ -1,8 +1,8 @@
 extends Character
 class_name TroopUnit
 
-@onready var navigation_agent: NavigationAgent2D = get_node("NavigationAgent2D")
 @onready var path_timer: Timer = get_node("PathTimer")
+@onready var navigation_agent: NavigationAgent2D = get_node("NavigationAgent2D")
 
 @export var damage: int = 1
 @export var knockback_force: int = 300
@@ -17,22 +17,21 @@ func chase() -> void:
 func _get_path_to_enemy() ->void:
 	navigation_agent.target_position = target.position
 
-func _on_path_timer_timeout() -> void:
-#	Verifico si el player esta en memoria, si no esta es que fue eliminado y busco otro.
-	if is_instance_valid(target):
-		target = get_closest_node(GLOBAL.get_unit_enemies(isTeamOne))
-		_get_path_to_enemy()
-	else:
-		target = get_closest_node(GLOBAL.get_unit_enemies(isTeamOne))
-
 func get_closest_node(nodes: Array) -> Node:
 	var closest_node: Node = null
 	var min_distance: float = INF
 	
 	for node in nodes:
-		if node and node is Node2D:  # Asegúrate de que el nodo es un Node2D
+		if node and node is Node2D:  # Me aseguro de que el nodo es un Node2D
 			var distance = global_position.distance_to(node.global_position)
 			if distance < min_distance:
 				min_distance = distance
 				closest_node = node
 	return closest_node
+	
+func _on_path_timer_timeout() -> void:
+	if is_instance_valid(target):
+		target = get_closest_node(GLOBAL.get_unit_enemies(isTeamOne))
+		_get_path_to_enemy()
+	else:
+		target = get_closest_node(GLOBAL.get_unit_enemies(isTeamOne))
