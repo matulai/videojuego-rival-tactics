b92b8263e453b1f8bc35f31cb2f98a510caf67c7
Matias Laime <matulai1221@gmail.com>
2024-10-28 20:00:33 -0300

    Fix bugs

--- boss_rival_tactics/characters/soldier/states/ChaseState.gd
+++ boss_rival_tactics/characters/soldier/states/ChaseState.gd
@@ -1,16 +1,19 @@
 extends AbstractState
 
 func update(delta: float) -> void:
-		character.chase()
-		character.move()
-		if is_instance_valid(character.target):
-			if character.navigation_agent.is_target_reached():
-				character._play_animation("attack_animation")
-		else:
-			emit_signal("finished", "idle")
+	character.chase()
+	character.move()
+
+	if is_instance_valid(character.target):
+#		Ataco si el enemigo esta cerca, distancia definida en NavigationAgent2D
+		if character.navigation_agent.is_target_reached():
+			character._play_animation("attack_animation")
+	else:
+		emit_signal("finished", "idle")
 
 
 func _on_animation_finished(anim_name: String) -> void:
+#CUANDO SE AGREGEN MAS ANIMACIONES CAMBIAR ESTA IMPLEMENTACION
 	if character.navigation_agent.is_target_reached():
 		character.target.take_damage(
 			character.damage, 
