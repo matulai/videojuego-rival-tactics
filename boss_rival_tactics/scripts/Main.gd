b40f36e9638323691b276421e3d9e260df6018d9
Matias Laime <matulai1221@gmail.com>
2024-11-01 16:26:35 -0300

    Se crea menu principal con musica de fondo y botones

--- boss_rival_tactics/scripts/Main.gd
+++ boss_rival_tactics/scripts/Main.gd
@@ -1,5 +1,6 @@
 extends Node
 
+# Nodos de unidades de cada bando
 @onready var player_one_army = get_node("PlayerOneArmyNode")
 @onready var player_two_army = get_node("PlayerTwoArmyNode")
 
@@ -8,7 +9,14 @@ extends Node
 @export var unit_scenes_right: Array[PackedScene]
 
 # Tiempo en segundos entre unidades
-@export var spawn_interval: float = 7.0
+@export var spawn_interval: float = 5.0
+
+# Jugadores
+@export var player_one_scene: PackedScene
+@export var player_two_scene: PackedScene
+
+# Variable para spawnear o no a los jugadores
+@export var should_spawn_players: bool = false
 
 # Lugares donde apareceran las unidades
 @onready var right_top_spawn_position: Marker2D = $UnitsSpawner/RightTop
@@ -16,9 +24,19 @@ extends Node
 @onready var left_top_spawn_position: Marker2D = $UnitsSpawner/LeftTop
 @onready var left_bottom_spawn_position: Marker2D = $UnitsSpawner/LeftBottom
 
+# Lugares donde apareceran los jugadores
+@onready var player_one_spawn: Marker2D = $PlayersSpawner/PlayerOneSpawn
+@onready var player_two_spawn: Marker2D = $PlayersSpawner/PlayerTwoSpawn
+
+# Musica de fondo
+@onready var audio_stream_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
 var spawn_timer = 0.0
 
 func _ready() -> void:
+#	Pongo a reproducir la musica de fondo
+	audio_stream_player.play()
+	
 	# Obtener los enemigos (tropas) de cada bando
 	var player_one_enemies = player_two_army.get_children()
 	var player_two_enemies = player_one_army.get_children()
@@ -26,6 +44,8 @@ func _ready() -> void:
 	# Setear los enemigos en el autoload
 	GLOBAL.set_player_one_enemies(player_one_enemies)
 	GLOBAL.set_player_two_enemies(player_two_enemies)
+	
+	spawn_players()
 
 func _process(delta):
 	# Incrementar el temporizador
@@ -35,7 +55,7 @@ func _process(delta):
 		spawn_unit("left")
 		spawn_unit("right")
 
-func spawn_unit(side: String):
+func spawn_unit(side: String) -> void:
 	# Elegir posición aleatoria de spawn para el lado especificado
 	if side == "left":
 		# Elegir una escena de unidad aleatoria
@@ -44,7 +64,7 @@ func spawn_unit(side: String):
 		unit.position = left_top_spawn_position.position if randi() % 2 == 0 else left_bottom_spawn_position.position
 		unit.isTeamOne = true
 		GLOBAL.add_unit_to_army_one(unit)
-		add_child(unit)
+		player_one_army.add_child(unit)
 	elif side == "right":
 		# Elegir una escena de unidad aleatoria
 		var random_scene = unit_scenes_right[randi() % unit_scenes_right.size()]
@@ -52,4 +72,28 @@ func spawn_unit(side: String):
 		unit.position = right_top_spawn_position.position if randi() % 2 == 0 else right_bottom_spawn_position.position
 		unit.isTeamOne = false
 		GLOBAL.add_unit_to_army_two(unit)
-		add_child(unit)
+		player_two_army.add_child(unit)
+
+func spawn_players() -> void:
+	# Instanciar el jugador uno y establecer su posición
+	if should_spawn_players:
+		var player_one = player_one_scene.instantiate()
+		player_one.position = player_one_spawn.position
+		player_one.is_player_one = true
+		player_one.isTeamOne = true
+#		temporal
+		player_one.damage = 1
+#
+		player_one_army.add_child(player_one)
+		GLOBAL.add_unit_to_army_one(player_one)
+
+		# Instanciar el jugador dos y establecer su posición
+		var player_two = player_two_scene.instantiate()
+		player_two.position = player_two_spawn.position
+		player_two.is_player_one = false
+		player_two.isTeamOne = false
+#		temporal
+		player_two.damage = 1
+#
+		player_two_army.add_child(player_two)
+		GLOBAL.add_unit_to_army_two(player_two)
