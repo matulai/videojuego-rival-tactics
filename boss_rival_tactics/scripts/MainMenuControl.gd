3e640697dfc3dda98f20888667805e6a30936cee
Matias Laime <matulai1221@gmail.com>
2024-11-01 00:10:38 -0300

    Se crea escena de menu principal y su script

---
+++ boss_rival_tactics/scripts/MainMenuControl.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/Game.tscn")
+
+func _on_options_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_credits_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_exit_button_pressed() -> void:
+	pass # Replace with function body.
