10e350082d9b4474718b1b6bbe87510dce643f9c
Matias Laime <matulai1221@gmail.com>
2024-11-21 15:27:24 -0300

    Se agregan animaciones y sonidos al soldier

--- boss_rival_tactics/characters/soldier/states/AttackState.gd
+++ boss_rival_tactics/characters/soldier/states/AttackState.gd
@@ -5,9 +5,21 @@ func enter() -> void:
 	character.move()
 	character._play_animation("attack_animation")
 
-func exit() -> void:
-	character.collision_shape.set_disabled(true)
-
 func update(_delta: float) -> void:
-	if not character.is_attacking:
+	character.chase()
+	if is_instance_valid(character.target) and character.is_target_reached():
+		#print(character.navigation_agent.is_target_reached())
+		character._play_animation("attack_animation")
+	else:
 		emit_signal("finished", "chase")
+
+func _on_animation_finished(anim_name: String) -> void:
+	if is_instance_valid(character.target) and anim_name == "attack_animation":
+		character.target.take_damage(
+			character.damage, 
+			character.knockback_direction, 
+			character.knockback_force
+		)
+
+func _on_soldier_character_hurt() -> void:
+	emit_signal("finished", "hurt")
--- boss_rival_tactics/characters/soldier/states/ChaseState.gd
+++ boss_rival_tactics/characters/soldier/states/ChaseState.gd
@@ -1,22 +1,16 @@
 extends AbstractState
 
-func update(delta: float) -> void:
-	character.chase()
-	character.move()
+func enter() -> void:
+	character._play_animation("walk_animation")
 
+func update(delta: float) -> void:
 	if is_instance_valid(character.target):
-#		Ataco si el enemigo esta cerca, distancia definida en NavigationAgent2D
+		character.chase()
+		character.move()
 		if character.navigation_agent.is_target_reached():
-			character._play_animation("attack_animation")
+			emit_signal("finished", "attack")
 	else:
 		emit_signal("finished", "idle")
 
-
-func _on_animation_finished(anim_name: String) -> void:
-#CUANDO SE AGREGEN MAS ANIMACIONES CAMBIAR ESTA IMPLEMENTACION
-	if is_instance_valid(character.target):
-		character.target.take_damage(
-			character.damage, 
-			character.knockback_direction, 
-			character.knockback_force
-		)
+func _on_soldier_character_hurt() -> void:
+	emit_signal("finished", "hurt")
