d774838beb54a0e7f34044774907782163076b5c
Matias Laime <matulai1221@gmail.com>
2024-11-21 15:26:23 -0300

    Agregacion de mas estados en player

--- boss_rival_tactics/characters/player/states/IdleState.gd
+++ boss_rival_tactics/characters/player/states/IdleState.gd
@@ -1,7 +1,15 @@
 extends AbstractState
 
+func enter() -> void:
+	character._play_animation("idle_animation")
+
 func update(delta: float) -> void:
 	character.handle_input()
 	if character.move_direction != Vector2.ZERO:
-		# Y cambiamos el estado a walk
 		emit_signal("finished", "walk")
+	if character.is_attack_pressed():
+		emit_signal("finished", "attack")
+
+
+func _on_player_character_hurt() -> void:
+	emit_signal("finished", "hurt")
--- boss_rival_tactics/characters/player/states/WalkState.gd
+++ boss_rival_tactics/characters/player/states/WalkState.gd
@@ -1,8 +1,16 @@
 extends AbstractState
 
+func enter() -> void:
+	character._play_animation("walk_animation")
+
 func update(delta: float) -> void:
 	character.handle_input()
 	if character.move_direction == Vector2.ZERO:
-		# Y cambiamos el estado a walk
 		emit_signal("finished", "idle")
+	if character.is_attack_pressed():
+		emit_signal("finished", "attack")
 	character.move()
+
+
+func _on_player_character_hurt() -> void:
+	emit_signal("finished", "hurt")
