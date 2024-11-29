9bdef0e5b914fceef56564c9ad307a16930c9111
Matias Laime <matulai1221@gmail.com>
2024-11-29 14:51:37 -0300

    Nuevo estado de hurt del jugador, fix hurt IA

---
+++ boss_rival_tactics/characters/player/states/HurtState.gd
@@ -0,0 +1,18 @@
+extends AbstractState
+
+func enter() -> void:
+	character.move_direction = Vector2.ZERO
+	character.move()
+	if (character.hp <= 0):
+		emit_signal("finished", "death")
+	else:
+		character.hurt_sound()
+		character._play_animation("hurt_animation")
+
+func update(delta: float) -> void:
+	if (character.hp <= 0):
+		emit_signal("finished", "death")
+
+func _on_animation_finished(anim_name: String) -> void:
+	if anim_name == "hurt_animation":
+		emit_signal("finished", "idle")
